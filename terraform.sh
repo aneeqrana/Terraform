b651eda01ae1b13bec5833a9677a35a97c74bd7b
Ubuntu <ubuntu@ip-172-31-61-103.ec2.internal>
2023-02-28 17:34:09 +0000

    setup the repo on server

---
+++ terraform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install unzip
+
+wget https://releases.hashicorp.com/terraform/1.3.9/terraform_1.3.9_linux_amd64.zip
+
+unzip terraform_1.3.9_linux_amd64.zip
+
+sudo mv terraform /usr/local/bin/
+
+terraform --version 
